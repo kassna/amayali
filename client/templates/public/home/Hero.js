ca2611c263b9792b93d5c244990234fe61fa1e8a
Alejandro Henkel <alehenkel17@gmail.com>
2017-06-02 13:59:41 -0500

    add hero form and fix hero layout

--- client/templates/public/home/Hero.js
+++ client/templates/public/home/Hero.js
@@ -1,4 +1,22 @@
+let selectpicker = require('bootstrap-select');
+
 Template.Hero.onRendered(function () {
 	// Inialize parallax
 	$('.parallax-section').parallax();
+	/* Changed locale in flatpickr.js */
+	$(".select-date").flatpickr({
+		enableTime: true,
+		altInput: true,
+		altFormat: "F j, Y h:i K",
+		utc: true,
+		minDate: moment().add(1, 'days').valueOf(),
+		minuteIncrement: 15,
+		time_24hr: true,
+		disableMobile: true,
+		wrap: true
+	});
+	$('.selectpicker').selectpicker({
+	  style: 'btn-info',
+	  size: 4
+	});
 });
