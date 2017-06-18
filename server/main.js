cd2aaac9f8c08e68ea7f507eaeb145caa3bfc29d
Alejandro Henkel <alehenkel17@gmail.com>
2017-06-18 14:39:58 -0500

    add script to complete orders

--- server/main.js
+++ server/main.js
@@ -23,25 +23,18 @@ AccountsTemplates.configure({
 Meteor.startup(() => {
 	// script to verify time of orders
 
-	// // Get current date in ISO format
-	// let now = new Date();
-	// now = Date.parse(now);
-  //
-	// // Find all orders with status confirmed
-	// Orders.find({ status: 'confirmed' }).map(function(order) {
-	//     // If date has passed, the order is completed
-	//     if (moment(order.date, "MM/DD/YYYY h:mm a").valueOf() < now) {
-	//         Orders.update({ _id: order._id }, { $set: { status: 'completed' } });
-	//     }
-	// });
-  //
-	// // Find all orders with status pending
-	// Orders.find({ status: 'pending_payment' }).map(function(order) {
-	//     // If date has passed, the order is canceled
-	//     if (moment(order.date, "MM/DD/YYYY h:mm a").valueOf() < now) {
-	//         Orders.update({ _id: order._id }, { $set: { status: 'canceled' } });
-	//     }
-	// });
+	// Get current date in ISO format
+	let now = new Date();
+	now = Date.parse(now);
+
+	// Find all orders with status confirmed
+	// TODO: Send survey email
+	Orders.find({ status: 'confirmed' }).map(function(order) {
+	    // If date has passed, the order is completed
+	    if (moment(order.date, "MM/DD/YYYY h:mm a").valueOf() < now) {
+	        Orders.update({ _id: order._id }, { $set: { status: 'completed' } });
+	    }
+	});
 
 
     Mailer.init({
