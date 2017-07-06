b1034b87808bc1b6055f1fcfbee94386d3e2763d
Alejandro Henkel <alehenkel17@gmail.com>
2017-07-05 19:42:37 -0500

    refactor datepicker init to a funcion used by two files

--- client/templates/clients/BookNow.js
+++ client/templates/clients/BookNow.js
@@ -56,35 +56,7 @@ Template.BookNowClient.onCreated(function () {
 });
 
 Template.BookNowClientForm.onRendered(() => {
-	let date = Session.get('date') || "";
-  // Set active inputs
-  if(date) {
-    // Set as default date
-    date = new Date(date);
-    $("[name='date']").parent().addClass('input--filled');
-  }
-	$(".select-date").flatpickr({
-		enableTime: true,
-		altInput: true,
-		altFormat: "F j, Y h:i K",
-    altInputClass: "",
-    defaultDate: date,
-		minDate: moment().add(3, 'h').valueOf(),
-		minuteIncrement: 15,
-		disableMobile: true,
-		wrap: true,
-    onChange: (selectedDates, dateStr) => {
-      Session.set('date', moment(dateStr).format("MM/DD/YYYY h:mm a"));
-    },
-    onOpen: (selectedDates, dateStr, instance) => {
-        $(instance.element).addClass('input--filled');
-    },
-    onClose: (selectedDates, dateStr, instance) => {
-       if(dateStr.trim() === '') {
-         $(instance.element).removeClass('input--filled');
-       }
-    }
-	});
+	datepickerSetup();
 });
 
 Template.BookNowClient.helpers({
