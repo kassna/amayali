1e80f0c1c2f86ffb5fa838f0d41d4135aefe1b9d
Sergio Larraguibel <sergio@kassna.com>
2017-08-23 19:41:31 -0500

    Ajuste menor a HeroTherapist.js

--- client/templates/public/therapistsinfo/HeroTherapist.js
+++ client/templates/public/therapistsinfo/HeroTherapist.js
@@ -8,29 +8,7 @@ Template.HeroTherapist.onCreated(function () {
 });
 
 
-Template.HeroTherapist.onRendered(function () {
-	// Inialize parallax
-	$('.parallax-section').parallax();
-	/* Changed locale in flatpickr.js */
-	$(".select-date").flatpickr({
-		enableTime: true,
-		altInput: true,
-		altFormat: "F j, Y h:i K",
-    altInputClass: "",
-		minDate: moment().add(3, 'h').valueOf(),
-		minuteIncrement: 15,
-		disableMobile: true,
-		wrap: true,
-		onChange: (selectedDates, dateStr) => {
-      Session.set('date', moment(dateStr).format("MM/DD/YYYY h:mm a"));
-    },
-	});
-	$('.selectpicker').selectpicker({
-	  style: 'btn-info',
-	  size: 4
-	});
 
-});
 
 Template.Hero.events({
   'click .submit-landing': () => {
