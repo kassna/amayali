1178aae6b1b7fa1c99fc59076b1362b97a337ed8
Sergio Larraguibel <sergio@kassna.com>
2017-08-24 08:07:42 -0500

    Add TherapistIncome to layout

---
+++ client/templates/public/therapistsinfo/Income.js
@@ -0,0 +1,27 @@
+Template.Prices.events({
+  'click .massage-type': event => {
+    const type = $(event.target).attr('data-type');
+    Session.set('type', type);
+    FlowRouter.go('book');
+  }
+});
+
+Template.PricesRow.helpers({
+  rate60: () => {
+    const locationId = Session.get('locationPrices');
+    if (!locationId) return;
+    return `$${Locations.findOne(locationId).base_rate} MXN`;
+  },
+  rate90: () => {
+    const locationId = Session.get('locationPrices');
+    if (!locationId) return;
+    const rate = rate_90(Locations.findOne(locationId).base_rate);
+    return `$${rate} MXN`;
+  },
+  rate120: () => {
+    const locationId = Session.get('locationPrices');
+    if (!locationId) return;
+    const rate = rate_120(Locations.findOne(locationId).base_rate);
+    return `$${rate} MXN`;
+  },
+});
