746141ebbb59a7e46f32f4d7967f059d60d8fb97
Sergio Larraguibel <sergio@kassna.com>
2017-08-23 19:06:30 -0500

    Varios cambios para probar ajustes del HeroTherapist

---
+++ client/templates/public/therapistsinfo/HeroTherapist.js
@@ -0,0 +1,39 @@
+let selectpicker = require('bootstrap-select');
+
+Template.HeroTherapist.onCreated(function () {
+  const self = this;
+	self.autorun(function() {
+		self.subscribe('activeLocations');
+	});
+});
+
+
+Template.HeroTherapist.onRendered(function () {
+	// Inialize parallax
+	$('.parallax-section').parallax();
+	/* Changed locale in flatpickr.js */
+	$(".select-date").flatpickr({
+		enableTime: true,
+		altInput: true,
+		altFormat: "F j, Y h:i K",
+    altInputClass: "",
+		minDate: moment().add(3, 'h').valueOf(),
+		minuteIncrement: 15,
+		disableMobile: true,
+		wrap: true,
+		onChange: (selectedDates, dateStr) => {
+      Session.set('date', moment(dateStr).format("MM/DD/YYYY h:mm a"));
+    },
+	});
+	$('.selectpicker').selectpicker({
+	  style: 'btn-info',
+	  size: 4
+	});
+
+
+
+Template.HeroTherapist.events({
+  'click .submit-landing': () => {
+    FlowRouter.go('book');
+  }
+});
