2d0d7c893c9fae7f5cb2889746347b93a1ba4671
Sergio Larraguibel <sergio@kassna.com>
2017-09-23 14:33:35 -0500

    Agregar PromoCode en información desplegada de los clientes

--- client/templates/admin/clients/ClientInfo.js
+++ client/templates/admin/clients/ClientInfo.js
@@ -1,3 +1,7 @@
 Template.ClientInfo.helpers({
   currentClient: () => Clients.findOne(Session.get('viewId'))
+  let self = this;
+	self.autorun(function() {
+		self.subscribe('clientPromoCode');
+	});
 });
