a745616da6e340762a50ea97253fd616428c0fa4
Sergio Larraguibel <sergio@kassna.com>
2017-09-23 14:42:01 -0500

    Prueba para que se muestre PromoCode del cliente

--- client/templates/admin/clients/ClientInfo.js
+++ client/templates/admin/clients/ClientInfo.js
@@ -1,6 +1,9 @@
 Template.ClientInfo.helpers({
   currentClient: () => Clients.findOne(Session.get('viewId'))
-  let self = this;
+});
+
+Template.ClientInfo.onCreated(function () {
+	let self = this;
 	self.autorun(function() {
 		self.subscribe('clientPromoCode');
 	});
