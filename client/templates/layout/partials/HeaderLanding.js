edd1ed6a506336d22a6c6569504aa518963f7244
Sergio-PC\Sergio <sergio@kassna.com>
2018-02-12 17:57:42 -0600

    Test to change logo from white to color on scroll

--- client/templates/layout/partials/HeaderLanding.js
+++ client/templates/layout/partials/HeaderLanding.js
@@ -2,9 +2,11 @@ Template.HeaderLanding.onRendered(() => {
   let scrollNavbar = () => {
     if ($(window).scrollTop() > 60){
       $('.navbar.toChange').addClass('small-nav');
+      $('img').attr("src","/img/logosup.png");
     }
     else {
       $('.navbar.toChange').removeClass('small-nav');
+      $('img').attr("src","/img/logosupbco.png");
     }
   }
   scrollNavbar();
