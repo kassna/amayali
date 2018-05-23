57e2dd623b9a7f9c856eed0bf2a8d8aa09e1a58c
Sergio-PC\Sergio <sergio@kassna.com>
2018-05-23 17:23:29 -0500

    Add Landing pages for each type of massage

--- client/templates/public/home/Prices.js
+++ client/templates/public/home/Prices.js
@@ -1,10 +1,3 @@
-Template.Prices.events({
-  'click .massage-type': event => {
-    const type = $(event.target).attr('data-type');
-    Session.set('type', type);
-    FlowRouter.go('book');
-  }
-});
 
 Template.PricesRow.helpers({
   rate60: () => {
---
+++ client/templates/public/prenatal/HeroPrenatal.js
@@ -0,0 +1,47 @@
+let selectpicker = require('bootstrap-select');
+
+Template.HeroPrenatal.onCreated(function () {
+  const self = this;
+	self.autorun(function() {
+		self.subscribe('activeLocations');
+	});
+});
+
+
+Template.HeroPrenatal.onRendered(function () {
+	// Inialize parallax
+	$('.parallax-section').parallax();
+	/* Changed locale in flatpickr.js */
+	$(".select-date").flatpickr({
+		enableTime: true,
+		altInput: true,
+		altFormat: "F j, Y h:i K",
+    altInputClass: "",
+		minDate: moment().add(3, 'h').valueOf(),
+		minuteIncrement: 15,
+		disableMobile: true,
+		wrap: true,
+		onChange: (selectedDates, dateStr) => {
+      Session.set('date', moment(dateStr).format("MM/DD/YYYY h:mm a"));
+    },
+	});
+	$('.selectpicker').selectpicker({
+	  style: 'btn-info',
+	  size: 4
+	});
+
+	// Hack to fix select not showing all cities
+	$(".selectpicker").selectpicker('refresh');
+	setTimeout(function doSomething() {
+	    $(".selectpicker").selectpicker('refresh');
+	    if($(".dropdown-menu.inner").first().find('li').length <= 1) {
+	    	setTimeout(doSomething, 500);
+	    }
+	}, 500);
+});
+
+Template.HeroPrenatal.events({
+  'click .submit-landing': () => {
+    FlowRouter.go('book');
+  }
+});
