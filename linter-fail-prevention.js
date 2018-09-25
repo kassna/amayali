8e07e888e0c421789ac3a180f1261d2c39171b58
Matthias Bräuer <matthias.braeuer@arcor.de>
2018-09-26 00:15:33 +0200

    Add meteor build options and empty js file for preventing linting failure in gitlab-ci job

---
+++ linter-fail-prevention.js
@@ -0,0 +1,7 @@
+/**
+ * The purpose of this file is to prevent eslint to fail as long as
+ * there is no file to lint. As soon as there is a new file created
+ * that is not specified in .eslintignore this file should be removed.
+ *
+ * @author Matthias Bräuer <a01679521@itesm.mx>
+ */
