bede1a65d782075244b7a984b480bfb42d01f3d4
Matthias <matthias.braeuer@arcor.de>
2018-10-23 08:35:17 +0000

    13: Add views for active and inactive agents in admin view

--- client/templates/admin/agents/agentRequest.js
+++ client/templates/admin/agents/agentRequest.js
@@ -1,19 +1,19 @@
-Template.AgentRequest.events({
-	'click .edit-button': function () {
-		Session.set('editId', this._id);
-		Session.set('editMode', 1);
-		Meteor.setTimeout(function() {
-			$("#edit-modal").modal('show');
-		}, 500);
-	},
-	'click .toggle-status': function () {
-		Meteor.call('toggleStatusAgent', this._id);
-	},
-	'click .view-more-btn': function() {
-		Session.set('viewId', this._id);
-		Session.set('viewMode', 1);
-		Meteor.setTimeout(function() {
-			$("#agent-info").modal('show');
-		}, 500);
-	}
+Template.agentRequest.events({
+    'click .edit-button': function () {
+        Session.set('editId', this._id);
+        Session.set('editMode', 1);
+        Meteor.setTimeout(function () {
+            $('#edit-modal').modal('show');
+        }, 500);
+    },
+    'click .toggle-status': function () {
+        Meteor.call('toggleStatusAgent', this._id);
+    },
+    'click .view-more-btn': function () {
+        Session.set('viewId', this._id);
+        Session.set('viewMode', 1);
+        Meteor.setTimeout(function () {
+            $('#agent-info').modal('show');
+        }, 500);
+    }
 });
